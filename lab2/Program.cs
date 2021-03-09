9a0ab0917de51a287b5aaf1712e49116a43f2cf4
Nikita Bubriak <31805409+nikitabubriak@users.noreply.github.com>
2021-03-09 12:22:35 +0200

    setup

---
+++ lab2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/*
+ * Lab 2
+ * Variant 7
+ * 
+ * Реалізувати задачу «Піца». 
+ * В продажу маємо декілька різновидів піци. 
+ * Однак існують додаткові опції, різні ботики піци, додаткові наповнювачі (наприклад, гриби, томати, маслини, тощо). 
+ * Необхідно при продажу піци динамічно визначати кількість додаткових параметрів та вартість кінцевого продукту.
+ */
+
+/*
+ * Decorator is a structural design pattern 
+ * that lets you attach new behaviors to objects 
+ * by placing these objects inside special wrapper objects that contain the behaviors.
+ */
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
